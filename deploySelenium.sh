2d1b0b9fe7975c65dd1ec90b714e794e22b74c80
Panagiotis Papadopoulos <panpap@gmx.com>
2014-08-04 13:33:37 +0300

    Create deploySelenium.sh

---
+++ deploySelenium.sh
@@ -0,0 +1,16 @@
+wget http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip
+unzip chromedriver_linux*
+rm chromedriver_linux*
+sudo chmod a+x /usr/bin/chromedriver
+apt-get -f install
+sudo apt-get install python-pip
+sudo pip install selenium
+sudo pip install pyvirtualdisplay
+sudo mv chromedriver /usr/bin/
+sudo apt-get install libcurl3 libnspr4-0d libxss1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*; rm google-chrome*
+apt-get -f install
+sudo apt-get install xvfb
+sudo Xvfb :1 -screen 5 1024x768x8 &
+export DISPLAY=:1.5
