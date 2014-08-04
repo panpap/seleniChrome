03b7bb09ecc792b55f3451ab950457b6a21d0998
Panagiotis Papadopoulos <panpap@gmx.com>
2014-08-04 14:04:00 +0300

    Update deploySelenium.sh

--- deploySelenium.sh
+++ deploySelenium.sh
@@ -9,6 +9,7 @@ sudo pip install pyvirtualdisplay
 sudo mv chromedriver /usr/bin/
 sudo apt-get install libcurl3 libnspr4-0d libxss1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#wget http://95.31.35.30/chrome/pool/main/g/google-chrome-stable/google-chrome-stable_33.0.1750.152-1_amd64.deb
 sudo dpkg -i google-chrome*; rm google-chrome*
 apt-get -f install
 sudo apt-get install xvfb
